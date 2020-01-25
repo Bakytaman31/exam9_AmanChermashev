6d8022757fb3aa4967bfdb5376e1e46c9df5e864
Aman Chermashev <bakytaman228337@gmail.com>
2020-01-25 21:56:25 +0600

    Made routes in App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './App.css';
 import Navigation from "./components/Navigation/Navigation";
 import {Route, Switch} from "react-router-dom";
+import Contacts from "./containers/Contacts/Contacts";
+import AddContact from "./containers/AddContact/AddContact";
+import EditContact from "./containers/EditContact/EditContact";
 
 function App() {
     return (
         <div className="App">
             <Navigation/>
             <Switch>
-
+                <Route path="/" exact component={Contacts}/>
+                <Route path="/addContact" exact component={AddContact}/>
+                <Route path="/editContact/:id" component={EditContact}/>
             </Switch>
         </div>
     );
